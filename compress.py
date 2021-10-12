c570d59cb0fc4c2894de71b4b70a0b568e3314bb
han1018 <paul52841018@hotmail.com>
2021-10-12 16:16:53 +0800

    feat : 新增自動化進行找到過大檔案進行壓縮

--- compress.py
+++ compress.py
@@ -6,25 +6,39 @@ from PIL import Image
 tinify.key = "xspvWq8m7gyrZ4dNxRnlyt8mYNFgvyC0"
 
 
-# def compressAPI(picPath):
-#     path, format = os.path.splitext(picPath)
-#     source = tinify.from_file(picPath)
-#     source.to_file(path+'_compressed'+format)
+def compressAPI(picPath, fileFormat):
+    # path, format = os.path.splitext(picPath)
+    tmpPic = 't.'+fileFormat
+    os.rename(picPath, tmpPic)
+    source = tinify.from_file(tmpPic)
+    source.to_file(picPath)
+    os.remove(tmpPic)
 
 
 def getNeedCompressedFiles(path, format):
+    # 檔案處裡 Ref : https://medium.com/seaniap/python-%E4%BD%BF%E7%94%A8%E6%AA%94%E6%A1%88%E7%B3%BB%E7%B5%B1-eaecbe7001dd
     target = path+'/'
     res = []
-    for picPath in gb.glob(target+'*.'+format):
-        # img = Image.open(picPath)
-        # imgSize = img.size  # 檔案大小
-
+    allFiles = gb.glob(target+'*.'+format)
+    for picPath in allFiles:
         imgSize = os.path.getsize(picPath)/1024/1024  # 單位:Mb
         if(imgSize > 1):
             res.append(picPath)
     return res
 
 
-files = getNeedCompressedFiles('Picture', 'jpg')
-print(len(files))
+def main():
+    paths = ['Picture', 'Other']
+    types = ['png', 'jpg']  # HEIC not supported
+    for path in paths:
+        for type in types:
+            print(path+'/'+type+":")
+            files = getNeedCompressedFiles(path, type)
+
+            for picPath in files:
+                print(picPath)
+                compressAPI(picPath, type)
+
+
 # compressAPI('./Picture/1.簡暉恩.jpg')
+main()
